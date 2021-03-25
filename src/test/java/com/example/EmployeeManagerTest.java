0d276f6c000c64e856d7c3ba396180a0c5bed1d0
jessica <jessica.rodriguez@iths.se>
2021-03-25 15:49:00 +0100

    Uppgift 2, test 2

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -2,8 +2,13 @@ package com.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 class EmployeeManagerTest {
 
@@ -20,5 +25,26 @@ class EmployeeManagerTest {
                 .collect(Collectors.toList());
 
         assertThat(employees).containsExactly(new Employee("Gustav", 25000).getId());
+    }
+
+    @Test
+    @DisplayName("Test mocking")
+    void testMocking(){
+
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee( "Gustav", 25000),
+                new Employee("Jessica", 25000),
+                new Employee("Tahar", 25000)));
+
+        employeeRepository.findAll();
+        var actual = employeeRepository.findAll();
+        var employees   = actual.stream()
+                .filter(employee -> employee.getId().equals("Gustav"))
+                .map((employeeId)->employeeId.getId())
+                .collect(Collectors.toList());
+
+        assertThat(employees).containsExactly(new Employee("Gustav", 25000).getId());
+
     }
 }
